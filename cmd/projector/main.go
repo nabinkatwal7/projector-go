bf54e904b0314cf77809b7770fed3a6cace0b9af
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2024-04-16 14:33:58 +0545

    cli parsing

--- cmd/projector/main.go
+++ cmd/projector/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main(){
-	
+import (
+	"fmt"
+	"log"
+
+	"github.com/nabinkatwal7/projector-go/pkg/projector"
+)
+
+func main() {
+	opts, err := projector.GetOpts()
+
+	if err != nil {
+		log.Fatal("Unable to get options: %v", err)
+	}
+
+	fmt.Printf("opts: %+v\n", opts)
 }
